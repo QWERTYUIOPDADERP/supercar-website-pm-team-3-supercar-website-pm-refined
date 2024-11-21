f94cca9b8e2b44535d0e9859bd10d4717ed20e58
Matthew <matthew@thecastellofamily.com>
2024-11-20 16:18:25 -0800

    made the sidebar better
    made focus actually work:
    - allowed in sidebar when sidebar open, but not when sidebar closed
    - set focus to start at top of sidebar when sidebar opened
    - made sidebar close when clicking off of the sidebar

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function openNav() {
-    document.getElementById("sidebar").style.width = "275px";
-    document.getElementById("header").style.marginLeft = "275px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("header").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-  }
+  document.getElementById("sidebar").style.width = "25%";
+  // document.getElementById("body").style.marginLeft = "25%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("sidebar").style.visibility="visible";
+  setTimeout(() => {
+    document.getElementById("close").focus();
+    clearTimeout();
+    }, 500); 
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  // document.getElementById("header").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+  // setTimeout(() => {
+  document.getElementById("sidebar").style.visibility="hidden";  
+  // }, 500);
+}
