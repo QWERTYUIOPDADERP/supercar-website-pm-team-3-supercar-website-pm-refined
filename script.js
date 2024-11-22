f53f1fa0516cebbf6e5832de36dc16a59a13f0bc
Matthew <matthew@thecastellofamily.com>
2024-11-22 15:37:21 -0800

    nav and footer finalized, hero image added for context

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function openNav() {
     }, 500); 
 }
 
+function openNavWithFocus(focus) {
+  document.getElementById("sidebar").style.width = "25%";
+  // document.getElementById("body").style.marginLeft = "25%";
+  document.getElementById("sidebar").style.visibility="visible";
+  setTimeout(() => {
+    document.getElementById(focus).focus();
+    clearTimeout();
+    }, 500); 
+}
+
 function closeNav() {
   document.getElementById("sidebar").style.width = "0";
   // document.getElementById("header").style.marginLeft= "0";
